ae97ac7825f086b2f5060babc1d7124eea931362
Bryan Benavides <104537354+Rainydata@users.noreply.github.com>
2025-11-08 23:56:37 -0500

    feat(api): Añade metodo de busqueda por medio el correo

--- backend/sistema-ventas/src/main/java/com/konex/loteria/sistemaventas/repository/ClienteRepository.java
+++ backend/sistema-ventas/src/main/java/com/konex/loteria/sistemaventas/repository/ClienteRepository.java
@@ -2,11 +2,12 @@ package com.konex.loteria.sistemaventas.repository;
 
 import com.konex.loteria.sistemaventas.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface ClienteRepository  extends JpaRepository<Cliente, Long> {
 
     boolean existsByCorreo(String correo);
+
+    java.util.Optional<Cliente> findByCorreo(String correo);
     
 }
