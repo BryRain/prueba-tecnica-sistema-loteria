7c53e5a5c2fde266318b1b0bec50d6d49136e80e
Bryan Benavides <104537354+Rainydata@users.noreply.github.com>
2025-11-09 12:02:44 -0500

    fix(config): habilitar los CORS para las peticiones del frontEnd

--- backend/src/main/java/com/konex/loteria/sistemaventas/controller/ClienteController.java
+++ backend/src/main/java/com/konex/loteria/sistemaventas/controller/ClienteController.java
@@ -1,16 +1,24 @@
 package com.konex.loteria.sistemaventas.controller;
 
-import com.konex.loteria.sistemaventas.dto.ClienteCreacionDTO;
-import com.konex.loteria.sistemaventas.model.Cliente;
-import com.konex.loteria.sistemaventas.service.ClienteService;
-import com.konex.loteria.sistemaventas.service.BilleteService;
-import com.konex.loteria.sistemaventas.dto.HistorialBilleteDTO;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.konex.loteria.sistemaventas.dto.ClienteCreacionDTO;
+import com.konex.loteria.sistemaventas.dto.HistorialBilleteDTO;
+import com.konex.loteria.sistemaventas.model.Cliente;
+import com.konex.loteria.sistemaventas.service.BilleteService;
+import com.konex.loteria.sistemaventas.service.ClienteService;
+
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/clientes")
 public class ClienteController {
--- backend/src/main/java/com/konex/loteria/sistemaventas/controller/SorteoController.java
+++ backend/src/main/java/com/konex/loteria/sistemaventas/controller/SorteoController.java
@@ -1,15 +1,22 @@
 package com.konex.loteria.sistemaventas.controller;
 
-import com.konex.loteria.sistemaventas.dto.SorteoCreacioDTO;
-import com.konex.loteria.sistemaventas.model.Sorteo;
-import com.konex.loteria.sistemaventas.service.SorteoService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.konex.loteria.sistemaventas.dto.SorteoCreacioDTO;
+import com.konex.loteria.sistemaventas.dto.SorteoRespuestaDTO;
+import com.konex.loteria.sistemaventas.model.Sorteo;
+import com.konex.loteria.sistemaventas.service.SorteoService;
+
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/sorteos")
 public class SorteoController {
@@ -37,8 +44,8 @@ public ResponseEntity<Sorteo> crearSorteo(@RequestBody SorteoCreacioDTO sorteoDT
  */
 
 @GetMapping
-public ResponseEntity<List<Sorteo>> listarSorteos(){
-    List<Sorteo> sorteos = sorteoService.listarSorteos();
+public ResponseEntity<List<SorteoRespuestaDTO>> listarSorteos(){
+    List<SorteoRespuestaDTO> sorteos = sorteoService.listarSorteos();
     return new ResponseEntity<>(sorteos, HttpStatus.OK);
 }
 
