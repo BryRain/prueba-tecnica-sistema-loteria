18097b26bdb0f2f46fe3323f77be545e1cd5e7fb
Bryan Benavides <104537354+Rainydata@users.noreply.github.com>
2025-11-10 01:51:14 -0500

    feat(ui): definir interfaces cliente y de billete

--- frontend/sistemas-ventas-frontend/src/app/models/billete.model.ts
+++ frontend/sistemas-ventas-frontend/src/app/models/billete.model.ts
@@ -1,6 +1,13 @@
+export interface Cliente {
+  id: number;
+  nombre: string;
+  correo: string;
+}
+
 export interface Billete {
   id: number;
   numero: string;
   precio: number;
   estado: 'disponible' | 'vendido';
+  cliente?: Cliente; 
 }
