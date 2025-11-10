03ec66b119c84f837308e555ef6a17b2a170cfe5
Bryan Benavides <104537354+Rainydata@users.noreply.github.com>
2025-11-10 01:53:24 -0500

    feat(ui): configura el enrutamiento de la app

--- frontend/sistemas-ventas-frontend/src/app/app.routes.ts
+++ frontend/sistemas-ventas-frontend/src/app/app.routes.ts
@@ -5,12 +5,19 @@ import { SorteoListComponent } from './components/sorteo-list/sorteo-list';
 import { ClienteRegistroComponent } from './components/cliente-registro/cliente-registro';
 import { HistorialClienteComponent } from './components/historial-cliente/historial-cliente';
 
+// --- ¡CORRECCIÓN EN LA RUTA DEL ARCHIVO! ---
+// Debe coincidir con la convención de carpetas: minúsculas y guiones.
+import { SorteoDetalleComponent } from './components/sorteo-detalle/sorteo-detalle'; 
+
 export const routes: Routes = [
-  // --- 2. DEFINE LAS RUTAS ---
+    
   { path: 'sorteos', component: SorteoListComponent },
+
+  // Esta es la ruta dinámica para la "vista del sorteo" (Req. F-3)
+  { path: 'sorteos/:id', component: SorteoDetalleComponent },
+
   { path: 'registrar-cliente', component: ClienteRegistroComponent },
   { path: 'historial', component: HistorialClienteComponent },
 
-  // --- 3. RUTA POR DEFECTO ---
   { path: '', redirectTo: '/sorteos', pathMatch: 'full' }
 ];
